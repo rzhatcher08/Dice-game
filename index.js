5b18a91b0124605d12538d25b3e5d5b299426ec0
rzhatcher08 <18086575+rzhatcher08@users.noreply.github.com>
2019-05-17 19:29:48 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+
+var randomNumber1 = Math.floor((Math.random()* 6) + 1);
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";  // This is random string from dice 1 to dice 6 image
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png-dice6.png
+
+var image1 = document.querySelectorAll("img")[0]; //document.querySelectorAll returns a list representing all documents elements
+
+image1.setAttribute("src",randomImageSource);
+
+
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//if then statments
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
